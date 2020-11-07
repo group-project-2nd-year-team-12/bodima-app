3df848f51602877f842137e0e158b04c7bcdc623
NimashaSupunpraba <49788577+NimashaSupunpraba@users.noreply.github.com>
2020-11-07 11:26:15 +0530

    css possition change e1

--- views/postBoarding.php
+++ views/postBoarding.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 	<title>&#127969; Post Boarding</title>
-	<link href="../css/style.css" rel="stylesheet">
+	<link href="../resource/css/style.css" rel="stylesheet">
 
 	<style>
 
