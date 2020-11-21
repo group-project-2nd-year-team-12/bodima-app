6003754a3f6fc9d1d2fd2d752ecc8e38d20fcbb3
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2020-11-21 06:35:24 +0530

    access restricted- boarding post detailed

--- controller/adertisement_Control.php
+++ controller/adertisement_Control.php
@@ -1,8 +1,19 @@
 <?php   require_once ('../config/database.php');
-        require_once ('../models/advertisement_model.php');
+        require_once ('../models/adertisement_model.php');
         session_start(); 
 ?>
 
 
+<?php
 
+echo $_GET['id'];
+
+if(isset($_SESSION['email'])){
+    header('Location:../views/boardingpage_detailed.php?id='.$_GET['id']);
+}
+// else{
+//     header('Location:../views/boardings.php
+// }
+
+?>
 
