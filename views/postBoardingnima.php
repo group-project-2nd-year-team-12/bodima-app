d171dfc53a27da20107c8387460bdab6052e2556
NimashaSupunpraba <49788577+NimashaSupunpraba@users.noreply.github.com>
2021-01-07 01:27:46 +0530

    edit boarding form

--- views/postBoardingnima.php
+++ views/postBoardingnima.php
@@ -1,72 +1,80 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title>postboarding</title>
-    <link rel="stylesheet" href="../resource/css/new_home.css"> 
-	<link rel="stylesheet" href="../resource/css/all.css">
-    <link rel="stylesheet" href="../resource/css/extra.css">
-    <link href="../resource/css/boarding.css" rel="stylesheet">
+	<title>&#127969; Post Boarding</title>
+	<!-- <link rel="stylesheet" href="../resource/css/new_home.css"> -->
+	<!-- <link rel="stylesheet" href="../resource/css/all.css"> -->
+	<link href="../resource/css/style1.css" rel="stylesheet">
+	
+
+	<style>
+
+	</style>
 </head>
-<body>
+<body class="boarding">
 
 <?php require "header1.php"?>
 
-<div class="con-1">
-<?php
-        if(isset($_GET['param']))
-        {
-            $errors=$_GET['param'];
-            /*
-            foreach($errors as $error)
-            {
-                echo '<p class="error"><b>'.$error.'</b></p>';
-            }*/
-            //print_r($errors);
-        }
-        
-        ?>
-    
 
-    
+
+	<div class="sub-container" id="img-sub">
+				<div><img src="../resource/icons/boarder/boarding/Pay Rent.png" alt="logo" class="verticle-center" width=50 height=auto  /></div>
+		
+
+		<div class="postBoarding"><h1>Boarding Advertisement Form</h1></div><!-- postBoarding -->
+
+	</div>
 	
-	<form action="../controller/postBoardingCon.php" method="post" enctype="multipart/form-data"  class="form">
+	
+
+		<div class=main>
+
+		<div class='main-new'>
+
+<div class=error-post>
+	<?php
+	if(isset($_GET['param']))
+	{
+		$errors=$_GET['param'];
+		/*
+		foreach($errors as $error)
+		{
+			echo '<p class="error"><b>'.$error.'</b></p>';
+		}*/
+		//print_r($errors);
+	}
+	
+	?>
+	</div>	
+		
+	</div><!--  main-new -->
+
+
+			<div class="part1">
+	
+			<form action="../controller/postBoardingCon.php" method="post" enctype="multipart/form-data"  id="postBoarding">
 			<!-- <div id="name"> -->
 
-    <h1>Boarding Advertisement Form</p><!-- postBoarding -->
-    <hr/>
-
-    <div class="section">
-                    <p>Title</p>
-                    <input type="text" name="title" id="title" ><br>
-                    
-                    <p>Description </p >
-                    <!--<input type="text" name="description" id="description" placeholder="Enter Description" ><br><br>-->
-                    <textarea name="description" id="description" rows="3" cols="26"></textarea><br>
-    </div>
-
-    <hr/>
-    <div class="section">
-                    <!--<label for="">Address  </label><br>-->
-				<p>Address </p>
+				<!--<label for="">Address  </label><br>-->
+				<h3 class="name">Address </h3 >
 				
 				<input class="hnumber" type="text" name="Hnumber" id="Hnumber"  placeholder="House Number / Name">
 				<!-- <label class="hLable"> House Number / Name  </label><br> -->
 				<?php if(isset($errors['err1'])) echo "<div class='error_msg'>".$errors['err1']."</div>"; ?>
 
-				
+				<br>
 				<input class="lane" type="text" name="lane" id="lane" placeholder="Lane" >
 				<!-- <label class="lLable" > Lane </label><br> -->
 				<?php if(isset($errors['err2'])) echo "<div class='error_msg'>".$errors['err2']."</div>"; ?>
 
-				
+				<br>
 				
 				<input class="city" type="text" name="city" id="city" placeholder="City">
 				<!-- <label class="cityLable" > City  </label><br> -->
 				<?php if(isset($errors['err3'])) echo "<div class='error_msg'>".$errors['err3']."</div>"; ?>
 
-			
+				<br>
 				
 				<input class="district" type="text" name="district" id="district" placeholder="District" >
 				<!-- <label class="lDis" >District  </label><br> -->
@@ -75,45 +83,52 @@
 
 			<!-- </div>id name	 -->
 
-				<p>Location</p >
+				<h3 class="name">Location</h3 >
 				<!-- <label for="">Location  </label><br> -->
-                <input type="text" name="location" id="location" ><br>
-                
-
-    </div>
-
-
-    <hr/>
-    <div class="section">
-            <p>Boarding Cover Image</p ><br>
-				<input type="file" name="BCimage" accept=".jpg, .png, .jpeg"  id="BCimage" value=../resource/Images/uploaded_boarding/defaultbp1.jpg ><br>
-    </div>
-
-
-    <hr/>
-    <div class="section">
-            <h4>Boarding Information</h4>
-
-            <p>Renting For Girls Or Boys</p >
+				<input type="text" name="location" id="location" ><br>
 				
-					<input type="radio" name="gender" value="Boys"> <span>Boys</span>
-					<input type="radio" name="gender" value="Girls"> <span>Girls</span> 
+
+				<h3 class="name">Description </h3 >
+				<!--<input type="text" name="description" id="description" placeholder="Enter Description" ><br><br>-->
+				<textarea name="description" id="description" rows="3" cols="26"></textarea><br>
+
+				
+				<h3 class="name">Boarding Cover Image</h3 ><br>
+				<input type="file" name="BCimage" accept=".jpg, .png, .jpeg"  id="BCimage" value=../resource/Images/uploaded_boarding/defaultbp1.jpg ><br>
+				
+
+				<!--<label for="">Boarding Images  </label><br>
+				<input type="file" name="Bimage{}" id="Bimage" multiple ><br><br>-->
+				
+			
+				
+				</div>	
+
+			<div class="part2">
+
+
+			
+
+				<h3 class="name">Renting For Girls Or Boys</h3 >
+				<label class="radio">
+					<input type="radio" name="gender" value="Boys"> Boys
+					<input type="radio" name="gender" value="Girls"> Girls 
 					<?php if(isset($errors['err6'])) echo "<div class='error'>".$errors['err6']."</div>"; ?>
 
-				
+				</label>
 			
-			<p>Renting Options</p >
-				
-					<input type="radio" name="individual" id="individual" value="Individual"><span id="individual" >&nbsp; <span>Individual</span></span>&nbsp;&nbsp;
-					<input type="radio" name="individual" id="RoomOrHome" value="RoomOrHome"><span id="RomeOrHome" >&nbsp; <span>Rome Or Home</span></span>&nbsp;&nbsp;
+			<h3 class="name">Renting Options</h3 >
+				<label class="radio">
+					<input type="radio" name="individual" id="individual" value="Individual"><span id="individual" >&nbsp; Individual</span>&nbsp;&nbsp;
+					<input type="radio" name="individual" id="RoomOrHome" value="RoomOrHome"><span id="RomeOrHome" >&nbsp; Rome Or Home</span>&nbsp;&nbsp;
 					<?php if(isset($errors['err5'])) echo "<div class='error'>".$errors['err5']."</div>"; ?>
 
-				
+				</label>
 				
 
 				
 			
-				<p>Total Person Count</p >
+				<h3 class="name">Total Person Count</h3 >
 				<input type="number"  name="Pcount" id="pcount" value=1  >
 				<?php   if(isset($errors['err7'])){
 							echo "<div class='error2'>".$errors['err7']."</div>"; 
@@ -124,14 +139,9 @@
 						} 
 				?>
 
-    </div>
 
-                
-    <hr/>          
-    <div class="section">
-    <h4>Boarding Renting fee details</h4>
-
-            <p>Cost Per Person For Month</p >
+				
+				<h3 class="name">Cost Per Person For Month</h3 >
 				<input type="text" name="CPperson" id="cpperson"  >
 				<?php   if(isset($errors['err10'])){
 							echo "<div class='error2'>".$errors['err10']."</div>"; 
@@ -143,7 +153,7 @@
 				?>
 
 				
-				<p>KeyMoney</p >
+				<h3 class="name">KeyMoney</h3 >
 				<input type="text" name="Keymoney" id="Keymoney" >
 				<?php   if(isset($errors['err12'])){
 							echo "<div class='error2'>".$errors['err12']."</div>"; 
@@ -155,7 +165,7 @@
 				?>
 
 				<div class="group">
-				<p>Avertisement Lifespan (Days)</p >
+				<h3 class="name">Avertisement Lifespan (Days)</h3 >
 				
 				<input type="number"  name="Lifespan" id="lifespan" value=30  class="control prc" >
 				<?php   if(isset($errors['err14'])){
@@ -168,24 +178,19 @@
 				
 				<div class="group">
 				
-				<p>Avertisement Amount :     Rs  </p >
+				<h3 class="name">Avertisement Amount :     Rs  </h3 >
 				<!-- <output  name="result" id="result"></output>   -->
-				<input type="text"  disabled  name="Aamount" id="Aamount" value=3000 >
+				<input type="text"  disabled  name="Aamount" id="Aamount" value=3000 ><br><br>
 				</div>
-						
-				
-    </div>
-    <hr/>   
-    <div class="submitdiv">
-                <input type="submit" class="save" name="submit" id="submit" value="Save Details" >
-    </div>
-				
-
-
-				<!--<label for="">Boarding Images  </label><br>
-				<input type="file" name="Bimage{}" id="Bimage" multiple ><br><br>-->
+					
+				<br>
+				<label for="">&nbsp; </label><br>
+				<input type="submit" name="submit" id="submit" value="Save"  ><br>	<br><br>
 					
 						
+				
+			
+
 				<script src="../resource/js/jquery-3.5.1.min.js"></script>
 				<script>
 					$('.group').on('input','.prc',function(){
@@ -217,7 +222,9 @@
 				
 			</form>
 
-</div>
-    
+			</div>
+
+		<div><!-- main -->
+		
 </body>
 </html>
