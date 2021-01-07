33fea7e77f10e8a34fc001e715298dff8e37f048
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-01-07 11:59:03 +0530

    title changed

--- views/myboarders1.php
+++ views/myboarders1.php
@@ -8,7 +8,7 @@
 <head>
 	<meta charset="UTF-8">
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	 <title>profile</title>
+	 <title>myborders</title>
 	 <link rel="stylesheet" href="../resource/css/new_home.css">
 	 <link rel="stylesheet" href="../resource/css/all.css">
 	 <link rel="stylesheet" href="../resource/css/sidebar2.css">
